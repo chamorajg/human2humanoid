082cc70f6e9b11b57a5a7fd02e5a129bde13309e
chamorajg <chanduiyer.raja@gmail.com>
2024-10-30 15:09:32 -0700

    use variable names instead of indices.

--- phc/phc/utils/torch_stompy_humanoid_batch.py
+++ phc/phc/utils/torch_stompy_humanoid_batch.py
@@ -43,9 +43,11 @@ class Stompy_Batch:
         self.mjcf_data = mjcf_data = self.from_mjcf(mjcf_file)
         self.extend_hand = extend_hand
         self.extend_head = extend_head
+        self._arm_indices = [9, 18] # Indices of forearm
+        self._root_index = [0] # To which head shall be attached.
         if extend_hand:
             self.model_names = mjcf_data['node_names'] + ["left_hand_keypoint_link", "right_hand_keypoint_link"]
-            self._parents = torch.cat((mjcf_data['parent_indices'], torch.tensor([9, 18]))).to(device) # Adding the hands joints
+            self._parents = torch.cat((mjcf_data['parent_indices'], torch.tensor(self._arm_indices))).to(device) # Adding the hands joints
             arm_length = 0.3
             self._offsets = torch.cat((mjcf_data['local_translation'], torch.tensor([[arm_length, 0, 0], [arm_length, 0, 0]])), dim = 0)[None, ].to(device)
             self._local_rotation = torch.cat((mjcf_data['local_rotation'], torch.tensor([[1, 0, 0, 0], [1, 0, 0, 0]])), dim = 0)[None, ].to(device)
@@ -59,7 +61,7 @@ class Stompy_Batch:
         if extend_head:
             self._remove_idx = 3
             self.model_names = self.model_names + ["head_link"]
-            self._parents = torch.cat((self._parents, torch.tensor([0]).to(device))).to(device) # Adding the hands joints
+            self._parents = torch.cat((self._parents, torch.tensor(self.root_index).to(device))).to(device) # Adding the hands joints
             head_length = 0.5
             self._offsets = torch.cat((self._offsets, torch.tensor([[[0, 0, head_length]]]).to(device)), dim = 1).to(device)
             self._local_rotation = torch.cat((self._local_rotation, torch.tensor([[[1, 0, 0, 0]]]).to(device)), dim = 1).to(device)
